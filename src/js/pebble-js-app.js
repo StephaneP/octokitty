8b0360aa8dddf2bf6ca1420fe1683b266dde1480
Stephane Paul <stephane.paul@gmail.com>
2014-04-08 23:39:12 -0400

    [CONFIGURATION] - enable configuration capability

--- src/js/pebble-js-app.js
+++ src/js/pebble-js-app.js
@@ -1,17 +1,52 @@
+var kitty = {
+	appMessageQueue: [],
+	appMessageRetryTimeout: 3000,
+	appMessageTimeout: 100,
+	repositories: {},
+	max_repositories: 20
+};
+
 Pebble.addEventListener("ready", function(e) {
-	if(localstorage.github_token){
-		//do github api stuff here
+	if(localStorage.github_token){
+		getUserAccountInfo();
 	}
 });
 
 Pebble.addEventListener("showConfiguration", function(e){
-	var callback_uri = "";
+	Pebble.openURL("https://github.com/login/oauth/authorize?client_id=6d2d277b89edf51f76ca");
+});
 
-	if(callback_uri){
-		Pebble.openURL("");
+Pebble.addEventListener("webViewClosed", function(e){
+	var result = JSON.parse(e.response);
+
+	if( result.success == true ){
+		localStorage.github_token = result.token;
+		notifyPebbleConnected(resul.token.toString());
+		// get repositories
+		// get issues
+		// get notifications
 	}else{
-		Pebble.showSimpleNotificationOnPebble("Octokitty", "Invalid authorization url");
+		Pebble.showSimpleNotificationOnPebble("Octokitty", "Connection Failed. Try Again.");
 	}
 });
 
+var notifyPebbleConnected = function(result, message){
+	var _appMessage = {
+		'result': ( result == true ) ? 1 : 0,
+		'name': message
+	};
 
+	var _success = function(e){
+		console.log('Successfully delivered token message with transactionId=' + e.data.transactionId);
+	};
+
+	var _error = function(e){
+		console.log('Unable to deliver token message with transactionId=' + e.data.transactionId);
+	};
+
+	var transactionId = Pebble.sendAppMessage(_appMessage, success, error);
+};
+
+var getUserAccountInfo = function(){
+
+};
--- src/octokitty.c
+++ src/octokitty.c
@@ -1,32 +1,25 @@
 #include <pebble.h>
 
+#define MAX_ISSUES 20
+#define MAX_REPOS 20
+#define KEY_TOKEN 10
+
+enum {
+  OCTO_KITTY_TOKEN = 0x01,
+  OCTO_KITTY_RESULT = 0x02,
+  OCTO_KITTY_REFRESH = 0x03,
+  OCTO_KITTY_ERROR = 0x04
+};
+
 static Window *window;
 static TextLayer *text_layer;
-
-static void select_click_handler(ClickRecognizerRef recognizer, void *context) {
-  text_layer_set_text(text_layer, "Select");
-}
-
-static void up_click_handler(ClickRecognizerRef recognizer, void *context) {
-  text_layer_set_text(text_layer, "Up");
-}
-
-static void down_click_handler(ClickRecognizerRef recognizer, void *context) {
-  text_layer_set_text(text_layer, "Down");
-}
-
-static void click_config_provider(void *context) {
-  window_single_click_subscribe(BUTTON_ID_SELECT, select_click_handler);
-  window_single_click_subscribe(BUTTON_ID_UP, up_click_handler);
-  window_single_click_subscribe(BUTTON_ID_DOWN, down_click_handler);
-}
 
 static void window_load(Window *window) {
   Layer *window_layer = window_get_root_layer(window);
   GRect bounds = layer_get_bounds(window_layer);
 
   text_layer = text_layer_create((GRect) { .origin = { 0, 72 }, .size = { bounds.size.w, 20 } });
-  text_layer_set_text(text_layer, "Press a button");
+  text_layer_set_text(text_layer, "Welcome to Octokitty!");
   text_layer_set_text_alignment(text_layer, GTextAlignmentCenter);
   layer_add_child(window_layer, text_layer_get_layer(text_layer));
 }
@@ -35,9 +28,67 @@ static void window_unload(Window *window) {
   text_layer_destroy(text_layer);
 }
 
+void showConnectedMessage(){
+  text_layer_set_text( text_layer, "Connected to Github!" );
+}
+
+void getUserAccountInfo(){
+  Tuplet refresh_tuple = TupletInteger(OCTO_KITTY_REFRESH, 1);
+  DictionaryIterator *iter;
+  app_message_outbox_begin(&iter);
+
+  if( iter == NULL ){
+    return;
+  }
+
+  text_layer_set_text(text_layer, "Getting Repositories");
+
+  dict_write_tuplet(iter, &refresh_tuple);
+  dict_write_end(iter);
+  app_message_outbox_send();
+}
+
+static void in_received_handler(DictionaryIterator *iter, void *context) {
+  Tuple *tuple_token = dict_find(iter, OCTO_KITTY_TOKEN);
+  Tuple *tuple_error = dict_find(iter, OCTO_KITTY_ERROR);
+
+  if( tuple_error ){
+    text_layer_set_text( text_layer, tuple_error->value->cstring );
+  }else if( tuple_token ){
+    text_layer_set_text( text_layer, "Connected to Github!" );
+    persist_write_string( KEY_TOKEN, tuple_token->value->cstring );
+
+    getUserAccountInfo();
+  }else{
+    text_layer_set_text( text_layer, "Connect to Github!" );
+  }
+}
+
+static void in_dropped_handler(AppMessageResult reason, void *context) {
+  APP_LOG(APP_LOG_LEVEL_DEBUG, "Incoming AppMessage from Pebble dropped, %d", reason);
+}
+
+static void out_sent_handler(DictionaryIterator *sent, void *context) {
+  // outgoing message was delivered
+}
+
+static void out_failed_handler(DictionaryIterator *failed, AppMessageResult reason, void *context) {
+  APP_LOG(APP_LOG_LEVEL_DEBUG, "Failed to send AppMessage to Pebble");
+}
+
+static void appmessage_init(void){
+  app_message_open(app_message_inbox_size_maximum(), app_message_outbox_size_maximum());
+  app_message_register_inbox_received(in_received_handler);
+  app_message_register_inbox_dropped(in_dropped_handler);
+  app_message_register_outbox_sent(out_sent_handler);
+  app_message_register_outbox_failed(out_failed_handler);
+}
+
 static void init(void) {
+  appmessage_init();
+
   window = window_create();
-  window_set_click_config_provider(window, click_config_provider);
+
   window_set_window_handlers(window, (WindowHandlers) {
     .load = window_load,
     .unload = window_unload,
